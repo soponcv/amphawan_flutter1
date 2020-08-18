62a482dcc9e6fbe6a90f10683aebbbd1787cdc79
harzancs <arzan.zan9@gmail.com>
2020-08-18 18:08:08 +0700

    zan 180863 1608

--- lib/system/pathAPI.dart
+++ lib/system/pathAPI.dart
@@ -1,7 +1,8 @@
-class PathAPI {
-  String base_url = 'http://amphawan.cityvariety.co.th/';
+final String _base_url = "http://amphawan.cityvariety.co.th/";
 
-  // get... => SELECT ,post... => INSERT ,update... => UPDATE ,del... => DELETE
+class PathAPI {
+  //ปฏิบัติธรรม
+  String getAllListDhamma = _base_url + '';
 
   PathAPI() : super();
 }
